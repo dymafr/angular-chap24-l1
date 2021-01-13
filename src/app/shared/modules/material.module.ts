93e7b34a0fdcba1194975a822bb90effae096df0
dymafr <erwan1309@hotmail.com>
2021-01-13 12:35:51 +0100

    2 files modified

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTabsModule } from "@angular/material/tabs";
 
-const MATERIALS = [MatButtonModule, MatCardModule];
+const MATERIALS = [
+  MatButtonModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule
+];
 
 @NgModule({
   imports: MATERIALS,
